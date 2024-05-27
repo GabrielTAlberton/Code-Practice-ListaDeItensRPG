4a7472eb42e15bfac2ef00527fef3f8c92ce8276
GabrielTAlberton <gtalberton@gmail.com>
2024-05-27 19:24:56 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// Lista para armazenar os itens
+List<string> itens = new List<string>();
+
+// TODO: Solicite os itens ao usuário
+for (int i = 0; i <=2; i++)
+{
+    Console.WriteLine("Digite o nome do item que deseja cadastrar:");
+    string nomeItem = Console.ReadLine();
+    itens.Add(nomeItem);
+}
+
+// Exibe a lista de itens
+Console.WriteLine("Lista de itens:");
+foreach (string item in itens)
+{
+    Console.WriteLine($"- {item}");
+}
